270ddb4fcf748a81956021abadc8e2e0979ab5c0
Bence Fekete <fekete965@gmail.com>
2025-09-04 19:41:17 +0100

    Add background image to the game

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import sys
 import pygame
 
-from constants import SCREEN_HEIGHT, SCREEN_WIDTH
+from constants import HALF_SCREEN_HEIGHT, HALF_SCREEN_WIDTH, SCREEN_HEIGHT, SCREEN_WIDTH
 from player import Player
 from asteroid import Asteroid
 from asteroidfield import AsteroidField
@@ -19,6 +19,17 @@ def main():
     clock = pygame.time.Clock()
     delta_time = 0
     
+    # Load the background
+    background_img = pygame.image.load('./assets/img/background.png')
+    # Scale the bg image up to half the screen width and height
+    background_img = pygame.transform.scale(background_img, (HALF_SCREEN_WIDTH, HALF_SCREEN_HEIGHT))
+    background_img_positions = [
+        (0, 0),
+        (HALF_SCREEN_WIDTH, 0),
+        (0, HALF_SCREEN_HEIGHT),
+        (HALF_SCREEN_WIDTH, HALF_SCREEN_HEIGHT),
+    ]
+
     # Setup the group
     asteroids = pygame.sprite.Group()
     drawable = pygame.sprite.Group()
@@ -37,24 +48,32 @@ def main():
     
     # Player setup
     Player.containers = (drawable, updatable)
-    
-    player_x = SCREEN_WIDTH / 2
-    player_y = SCREEN_HEIGHT / 2
+
+    player_x = HALF_SCREEN_WIDTH
+    player_y = HALF_SCREEN_HEIGHT
     player = Player(player_x, player_y)
 
     while True:
+        # Check for quite event
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
     
+        # Render background 
         screen.fill("black")
         
+        for bg_pos in background_img_positions:
+            screen.blit(background_img, bg_pos)
+        
+        # Update unit positions
         for unit in updatable:
             unit.update(delta_time)
-            
+
+        # Draw units
         for unit in drawable:
             unit.draw(screen)
         
+        # Check for collisions
         for asteroid_obj in asteroids:
             if asteroid_obj.is_colliding(player):
                 print("Game Over!")
@@ -65,6 +84,7 @@ def main():
                     asteroid_obj.split()
                     bullet_obj.kill()
         
+        # End the frame
         pygame.display.flip()
         delta_time = clock.tick(60) / 1000
 
